72e84b15652087e093ea402a93e9922018191a2f
DaNmarner <DaNmarner@gmail.com>
2010-05-31 00:43:22 -0500

    override default save() and delete() of Entry

--- sophie/models.py
+++ sophie/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
+import markdown
 
 class Blog(models.Model):
     title = models.CharField(max_length = 200)
@@ -31,20 +33,19 @@ class Entry(models.Model):
     PUBLISHED = 1
     HIDDEN = 2
     STATUS_CHOICES = (
-            (DRAFT, 'Draft'),
-            (PUBLISHED, 'Published'),
-            (HIDDEN, 'Hidden'),
+            (DRAFT_STATUS, 'Draft'),
+            (LIVE_STATUS, 'Live'),
+            (HIDDEN_STATUS, 'Hidden'),
     )
 
-    slug = models.SlugField(primary_key = True, uinque = True, 
-            max_length = 250)
+    slug = models.SlugField(uinque = True, max_length = 250)
     title = models.CharField(max_length = 250)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(default = datetime.datetime.now)
     last_update = models.DateTimeField()
     markup = models.PositiveIntegerField(default = MARKDOWN, 
             choices = MARKUP_CHOICES)
-    body = models.TextField()
-    body_html = models.TextField()
+    body = models.TextField(blank = True)
+    body_html = models.TextField(blank = True)
     teaser = models.TextField(blank = True)
     teaser_html = models.TextField(blank = True)
     status = models.PositiveIntegerField(defualt = DRAFT,
@@ -57,3 +58,27 @@ class Entry(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return ('post_view', (), { 'page_slug': self.slug })
+
+    def save(self, *args, **kwargs):
+        """ convert markup to html, book-keep category counter """ 
+
+        if not self.id: # newly created
+            self.pub_date = datetime.datetime.now()
+            self.category.count = self.category.count + 1
+            self.category.save()
+
+        self.last_updated = datetime.datetime.now()
+        self.body_html = markdown.markdown(self.body, ['codehilite'])
+
+        if self.teaser:
+            self.teaser_html = markdown.markdown(self.teaser, ['codehilite'])
+
+        super(Entry, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        """ counter-method of self.save, book-keeping category counter """
+
+        self.category.count = self.category.count - 1
+        self.category.save()
+
+        super(Entry, self).delete(*args, **kwargs)

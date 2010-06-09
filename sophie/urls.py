2074cccfd36a44dbe697274ed6bb4a8028837770
DaNmarner <DaNmarner@gmail.com>
2010-06-09 13:52:31 -0500

    add urls for feed

--- sophie/urls.py
+++ sophie/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
+# Page urls
 urlpatterns = patterns('sophie.views',
     url(r'^$', 'show_index', name='index_view'),
     url(r'^entries/$', 'list_entries'),
@@ -17,3 +18,9 @@ urlpatterns = patterns('sophie.views',
     url(r'^(?P<entry_slug>\w+)/$', 'show_entry', name='entry_view'),
 )
 
+# Feed urls
+urlpatterns+ = patterns('sophie.feeds',
+    url(r'^feed/$', 'BlogFeed', name='blog_feed'),
+    url(r'^category/(?P<category_slug>\w+)/feed/$', 
+        'CategoryFeed', name='blog_feed'),
+)

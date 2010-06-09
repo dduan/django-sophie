8f54256965e3724e47983d36f13f0ed4b98c5ce7
DaNmarner <DaNmarner@gmail.com>
2010-06-08 23:32:08 -0500

    factor out blog identification

---
+++ sophie/utils.py
@@ -0,0 +1,14 @@
+'''
+Utilities for django-sophie
+'''
+
+def get_blog(slug):
+    ''' 
+    get the blog entity identified by slug, if it doesn't
+    exist, get the one with the smallest id
+    '''
+    try:
+        return Blog.objects.get(slug=slug)
+    except Blog.DoesNotExist:
+        return Blog.objects.all()[0]
+
--- sophie/views.py
+++ sophie/views.py
@@ -1,14 +1,12 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render_to_response, get_object_or_404
 from sophie.models import Blog, Category, Entry
+from sophie.utils import get_blog
 import logging
 
 def list_entries(request, blog_slug=None, page_num=1):
     ''' renders requested page, a list of date-ordred entries '''
-    try:
-        blog = Blog.objects.get(slug=blog_slug)
-    except Blog.DoesNotExist:
-        blog = Blog.objects.all()[0]
+    blog = get_blog(blog_slug)
 
     pages = Paginator(Entry.live.all(), blog.entry_per_page)
     return render_to_response('list_entries.html', { 
@@ -23,10 +21,7 @@ def show_index(request, blog_slug=None):
 
 def show_category(request, blog_slug=None, category_slug=None, page_num=1):
     ''' lists entries under category specified by category_slug '''
-    try:
-        blog = Blog.objects.get(slug=blog_slug)
-    except Blog.DoesNotExist:
-        blog = Blog.objects.all()[0]
+    blog = get_blog(blog_slug)
     category = get_object_or_404(Category, slug=category_slug)
     pages = Paginator(Entry.live.filter(category=category),blog.entry_per_page)
     return render_to_response('category_view.html', { 
@@ -37,10 +32,7 @@ def show_category(request, blog_slug=None, category_slug=None, page_num=1):
         })
 
 def show_entry(request, entry_slug, blog_slug=None):
-    try:
-        blog = Blog.objects.get(slug=blog_slug)
-    except Blog.DoesNotExist:
-        blog = Blog.objects.all()[0]
+    blog = get_blog(blog_slug)
     entry = get_object_or_404(Entry, slug=entry_slug)
     return render_to_response('entry_view.html', { 
         'blog': blog,

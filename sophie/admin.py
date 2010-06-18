238d7507042525fef71be122a08a528ec2719448
DaNmarner <DaNmarner@gmail.com>
2010-06-18 01:14:21 -0500

    added save button on top in admin

--- sophie/admin.py
+++ sophie/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from sophie.models import Entry, Category, Blog
 
 class EntryAdmin(admin.ModelAdmin):
-    pass
+    save_on_top = True
 
 class CategoryAdmin(admin.ModelAdmin):
+    save_on_top = True
     readonly_fields = ('count',)
 
 class BlogAdmin(admin.ModelAdmin):

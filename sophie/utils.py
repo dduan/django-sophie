d3e9fc23857ad6822cbb0cd5a553336a2a7570a1
DaNmarner <DaNmarner@gmail.com>
2010-07-20 22:37:52 -0500

    put down the template routing rules

--- sophie/utils.py
+++ sophie/utils.py
@@ -8,6 +8,20 @@ from django.conf import settings
 # set multiblog_enabled = True to enable multi blog url routing
 multiblog_enabled = getattr(settings, 'SOPHIE_ENABLES_MULTIBLOG', False)
 
+def route_template(basename, extraname):
+    ''' 
+    Sophie's template locating logic in addition to Django's setting.
+
+    The given basename is looked for in the following order:
+
+        1.  sophie/[blog_slug]/[basename]_[extraname].html
+        2.  sophie/[blog_slug]/[basename].html
+        3.  sophie/default/[basename]_[extraname].html
+        4.  sophie/default/[basename].html
+
+    '''
+    pass
+
 class LaidbackPaginator(Paginator):
     '''
     Overwrites the original page method so it could be more

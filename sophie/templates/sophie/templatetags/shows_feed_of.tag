236d960a734aac459150a8bbfb532387e336efa2
DaNmarner <danmarner@gmail.com>
2010-07-28 15:10:05 -0500

    added shows_feed_of.tag

---
+++ sophie/templates/sophie/templatetags/shows_feed_of.tag
@@ -0,0 +1,5 @@
+<div>
+    <a href="{{ blog.get_feed }} ">
+        <h3>Feed</h3>
+    </a>
+</div>
--- sophie/templatetags/sophie_parts.py
+++ sophie/templatetags/sophie_parts.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.inclusion_tag('sophie/templatetags/lists_category_of.tag')
 def sophie_lists_category_of(blog):
     return {'category_list': blog.get_categories()}
+
+@register.inclusion_tag('sophie/templatetags/shows_feed_of.tag')
+def sophie_shows_feed_of(blog):
+    return {}

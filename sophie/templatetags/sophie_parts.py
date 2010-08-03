bdb56409e8ce4c6995c6f0c201d20c42095702f7
DaNmarner <danmarner@gmail.com>
2010-08-03 01:20:36 -0500

    used unicode as reverse() keywards

--- sophie/templatetags/sophie_parts.py
+++ sophie/templatetags/sophie_parts.py
@@ -16,7 +16,7 @@ def sophie_shows_feed_of(blog):
 @register.inclusion_tag('sophie/templatetags/links_siblings_of.tag')
 def sophie_links_siblings_of(page, blog, urlname, part_slug=None):
     # conditional operatior hack xx and yy or zz == xx ? yy : zz
-    url_bits = multiblog_enabled and { 'blog_slug': blog.slug } or {}
+    url_bits = multiblog_enabled and { u'blog_slug': blog.slug } or {}
 
     # urls are named following this convention:
     #   sophie_[part name]_[page type]_url
@@ -29,10 +29,10 @@ def sophie_links_siblings_of(page, blog, urlname, part_slug=None):
     # Note that previous_page_number() is dumb, it returns the number
     # regardless of whether that page exists, same with next_page_number.
     # So, this needs to be guarded in the template
-    url_bits['page_num'] = page.previous_page_number()
+    url_bits[u'page_num'] = unicode(page.previous_page_number())
     previous_link = reverse( urlname, kwargs=url_bits )
 
-    url_bits['page_num'] = page.next_page_number()
+    url_bits[u'page_num'] = unicode(page.next_page_number())
     next_link = reverse( urlname, kwargs=url_bits )
     
     return {

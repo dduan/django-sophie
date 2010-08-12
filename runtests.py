fb77f2e1207c744ae7120489fa800cdbec0f046f
DaNmarner <danmarner@gmail.com>
2010-08-12 00:56:10 -0500

    Added runtest.py
    Just go runtest.py without addtional args will run tests in Sophie with
    an in-memory database.

---
+++ runtests.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from django.conf import settings
+
+if not settings.configured:
+    print 'configuring settings'
+    settings.configure(
+        DATABASE_ENGINE = 'sqlite3',
+        SITE_ID = 1,
+        TEMPLATE_LOADERS = (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ),
+        MIDDLEWARE_CLASSES = (
+            'django.middleware.common.CommonMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+        ),
+        INSTALLED_APPS = (
+            'django.contrib.contenttypes',
+            'django.contrib.sites',
+            'django.contrib.sitemaps',
+            'sophie',
+        ),
+
+    )
+
+from os.path import dirname, abspath
+import sys
+
+from django.test.simple import run_tests
+
+def runtests(*test_args):
+    if not test_args:
+        test_args = ['sophie']
+    parent = dirname(abspath(__file__))
+    sys.path.insert(0, parent)
+    failures = run_tests(test_args)
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
+
+

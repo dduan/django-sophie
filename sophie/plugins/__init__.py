6fd8d0fe14471c760565b7d0892e4cb9f9f8abad
DaNmarner <danmarner@gmail.com>
2010-08-21 00:38:04 -0500

    Added a plugin regisration point.

---
+++ sophie/plugins/__init__.py
@@ -0,0 +1,10 @@
+class PluginMount(type):
+    def __init__(cls, name, bases, attrs):
+        if not hasattr(cls, 'plugins'):
+            cls.plugins = []
+        else:
+            cls.plugins.append(cls)
+
+
+class ActionProvider:
+    __metaclass__ = PluginMount
